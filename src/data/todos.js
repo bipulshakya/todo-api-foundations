394ec906beb054215c1bad4dc76393186d5473a3
bipulshakya <bipulshakya47@gmail.com>
2026-08-29 18:27:28 +0545

    Add Todo Data's

---
+++ src/data/todos.js
@@ -0,0 +1,33 @@
+const todos = [
+  {
+    id: 1,
+    title: "Node.js Basics",
+    notes: "Study variables and functions",
+    priority: "high",
+    done: false,
+  },
+  {
+    id: 2,
+    title: "Build Todo API",
+    notes: "Use Node.js and Express",
+    priority: "high",
+    done: false,
+  },
+  {
+    id: 3,
+    title: "Practice Git",
+    notes: "Practice commits and branches",
+    priority: "medium",
+    done: true,
+  },
+
+  {
+    id: 4,
+    title: "Learn Express.js",
+    notes: "Practice routing and middleware",
+    priority: "high",
+    done: false,
+  },
+];
+
+module.exports = todos;
