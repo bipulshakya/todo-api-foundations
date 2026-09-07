d901f72d94c13f375c1dede04ef43c427b88bd15
bipulshakya <bipulshakya47@gmail.com>
2026-09-07 20:18:32 +0545

    Add GET route to return all todos

--- src/routes/todos.routes.js
+++ src/routes/todos.routes.js
@@ -1,8 +1,15 @@
+//import the express module
 const express = require("express");
+
+//import the todos data
 const todos = require("../data/todos");
 
 //A built in methods in Express that creates a new isolated instance of middlewarew and routes.
 const router = express.Router();
 
+//GET /todos Return all todos
+router.get("/", (req, res) => {
+    res.status(200).json(todos);
+});
 
 module.exports = router;
