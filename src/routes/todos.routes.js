f6566ebc881cad5c3cb7e69c6fbf44726a1b994a
bipulshakya <bipulshakya47@gmail.com>
2026-09-07 20:00:28 +0545

    Setup Express Router

---
+++ src/routes/todos.routes.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const todos = require("../data/todos");
+
+//A built in methods in Express that creates a new isolated instance of middlewarew and routes.
+const router = express.Router();
+
+
+module.exports = router;
