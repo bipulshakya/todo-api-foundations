018c92a51d786a3911a0864518120c214c59e37e
bipulshakya <bipulshakya47@gmail.com>
2026-09-07 20:08:52 +0545

    Add comment for better understanding

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
+//import the express module
 const express = require("express");
+
+//This calls the express function and creates an Express application instance. This instance is used to configure the server, define routes, and handle incoming requests.
 const app = express();
 
 const PORT = 3000;
@@ -14,12 +17,7 @@ app.get("/health", (req, res) => {
     });
 });
 
-app.get("/", (req, res) => {
-    res.status(200).json({
-        message: "Hello World"
-    });
-});
-
+//mounts an express router on the /todos path. This means that any requests to /todos will be handled by the todosRoutes router.
 app.use("/todos", todosRoutes);
 
 
